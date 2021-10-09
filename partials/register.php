ea88b932c5a5932835c4baf25385cdc387602536
unknown <>
2021-10-09 16:16:15 +0200

    upgrade

--- partials/register.php
+++ partials/register.php
@@ -1,49 +1,79 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="../assets/css/login.css">
-  </head>
-  <body>
-    <div class="registerContainer">
-      <form class="" action="../registrationHandler.php" method="post">
-        <h2>Name</h2>
-        <input type="text" required name="registerName" value="">
-        <h2>Email</h2>
-        <input type="email" required name="registerEmail" value="">
-        <h2>Password</h2>
-        <input type="password" required name="registerPassword" value="">
 
-        <input type="radio" name="gender" value="musko" required> Musko
-        <input type="radio" name="gender" value="zensko"> Zensko <br><br>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="../assets/css/login.css">
+</head>
 
-        <div>
-        <p> Programski jezici</p>
-      
-        <p> Front End Developer </p>
-        <p> Angular </p>
-          <input type="checkbox" name="languanges[]" value="Angular_JS"> Angular_JS
-          <input type="checkbox" name="languanges[]" value="Angular_2"> Angular_2
-        <p> React </p>
-          <input type="checkbox" name="languanges[]" value="Angular_2"> Angular_2
-        <p> Vue </p>
-        <input type="checkbox" name="languanges[]" value="Vue"> Vue
+<body>
+  <div class="registerContainer">
+    <form class="" action="../registrationHandler.php" method="post">
+      <h2>Name</h2>
+      <input type="text" required name="registerName" value="">
+      <h2>Email</h2>
+      <input type="email" required name="registerEmail" value="">
+      <h2>Password</h2>
+      <input type="password" required name="registerPassword" value="">
 
+      <input type="radio" name="gender" value="musko" required> Musko
+      <input type="radio" name="gender" value="zensko"> Zensko <br><br>
 
-        <p> Back End Developer </p>
-        <p> PHP </p>
-          <input type="checkbox" name="languanges[]" value="Symfony"> Symfony
-          <input type="checkbox" name="languanges[]" value="Laravel"> Laravel
-          
-        <p> NODE_JS </p>
-          <input type="checkbox" name="languanges[]" value="Express"> Express
-          <input type="checkbox" name="languanges[]" value="NestJS"> NestJS
+      <div class="frameworksContainer">
 
+        <h3> Programski jezici</h3>
+
+        <div class="frameWorksBox">
+          <p> Front End Developer </p>
+          <input type="checkbox" name="stack_developer" value="frontend">
+        </div>
+        <div class="frameWorksBox">
+          <p> Angular </p>
+          <input type="checkbox" name="frameworks" value="Angular_JS">
+        </div>
+        <div class="frameWorksBox">
+          <p>Angular 2</p>
+          <input type="checkbox" name="frameworks" value="Angular_2">
+        </div>
+        <div class="frameWorksBox">
+          <p> React </p>
+          <input type="checkbox" name="frameworks" value="Angular_2">
+        </div>
+        <div class="frameWorksBox">
+          <p> Vue </p>
+          <input type="checkbox" name="frameworks" value="Vue">
         </div>
 
-        <button type="submit" name="button">Register</button>
-      </form>
-    </div>
-  </body>
+        <div class="frameWorksBox">
+          <p> Back End Developer </p>
+          <input type="checkbox" name="stack_developer" value="backend">
+        </div>
+        <div class="frameWorksBox">
+          <p> PHP </p>
+          <input type="checkbox" name="frameworks" value="PHP">
+        </div>
+        <div class="frameWorksBox">
+          <p> Symfony </p>
+          <input type="checkbox" name="frameworks" value="Symfony">
+        </div>
+        <div class="frameWorksBox">
+          <p> Laravel </p>
+          <input type="checkbox" name="frameworks" value="Laravel">
+        </div>
+        <div class="frameWorksBox">
+          <p> NODE_JS </p>
+          <input type="checkbox" name="frameworks" value="Express">
+        </div>
+        <div class="frameWorksBox">
+          <p>NestJS</p>
+          <input type="checkbox" name="frameworks" value="NestJS">
+        </div>
+      </div>
+
+      <button type="submit" name="button">Register</button>
+    </form>
+  </div>
+</body>
+
 </html>
--- searchHandler.php
+++ searchHandler.php
@@ -3,22 +3,29 @@
 
 $search = $_GET['searchQuerry']?? "";
 $sql= "SELECT users.name, users.email,frameworks.stack_developer,frameworks.frameworks
-        From users 
+        From users
         INNER JOIN frameworks
         ON frameworks.id = users.id
-        WHERE frameworks.frameworks = '$search'";
-         
+        WHERE frameworks.frameworks = '$search'
+        OR name='$search'
+        OR email='$search'
+        OR stack_developer='$search'
+        OR password='$search'";
+
 
 $query= mysqli_query($db,$sql);
-
-
 $result= mysqli_fetch_all($query);
 
-foreach ($result[0] as $value) {
-  echo "$value <br>";
+// var_dump($result[0]);
+if(isset($result[0])){
+  foreach ($result[0] as $value) {
+    echo "$value <br>";
+  }
+}else{
+  echo 'no result found';
 }
 
-  
+
   // $searchQuerry = $_POST['searchQuerry'];
   // $sql = "SELECT * FROM users WHERE name='$searchQuerry' OR password='$searchQuerry' OR email='$searchQuerry'";
   // $query = mysqli_query($db,$sql);
